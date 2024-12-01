765006eaeee7a206293ff6dc54196005cd8af494
noobCode24 <vusun2k2004@gmail.com>
2024-12-01 20:44:35 +0700

    basic layout

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,10 @@ import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 // Create a theme instance.
 // Theme dong nhat moi layout theo mot dang nhat quan
 const theme = extendTheme({
+  trelloCustom: {
+    appBarHeight: '48px',
+    boardBarHeight: '56px'
+  },
   colorSchemes: {
     light: {
       palette: {
