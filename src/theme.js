a60f3fa5f836cbe9e11c00f4f181e1c2a7315f18
noobCode24 <vusun2k2004@gmail.com>
2024-12-01 18:29:57 +0700

    update theme

--- src/theme.js
+++ src/theme.js
@@ -2,19 +2,22 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 // Create a theme instance.
+// Theme dong nhat moi layout theo mot dang nhat quan
 const theme = extendTheme({
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secondary: deepOrange
-      }
+      },
+      spacing: (factor) => `${0.25 * factor}rem` //dieu chinh gap mac dinh thanh 0.25 rem
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange
-      }
+      },
+      spacing: (factor) => `${0.25 * factor}rem` //dieu chinh gap mac dinh thanh 0.25
     }
   }
 })
